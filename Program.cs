d81df96ddc8180938e5ee5a8ab29058370879525
Argho Das <aiub2016@yahoo.com>
2021-10-02 00:54:07 +0600

    Cast Operations source code

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Cast_operations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           /* short num1 = 10;
+            short num2 = 20;
+
+            short sum = (short)(num1 + num2);
+
+            Console.WriteLine(sum);
+           */
+
+            
+
+        }
+
+        enum Gender
+        {
+            Male = 1,
+            Female = 2,
+            Others = 3
+        }
+        
+    }
+}
