a9501ff7956d75ec668adeb35484975ce5ea889f
Argho Das <aiub2016@yahoo.com>
2021-10-10 01:41:53 +0600

    Constructor 2 source code. the best example yet

---
+++ Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace constructor2
+{
+    class Book
+    {
+        public string title;
+        public string author;
+        public int pages;
+
+        public Book()
+        {
+
+        }
+
+        public Book(string aTitle, string aAuthor, int apages) //a = argument
+        {
+            // Console.WriteLine("Creating Books");
+            this.title = aTitle;
+            this.author = aAuthor;
+            this.pages = apages;
+        }
+    }
+}
---
+++ Constructors.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace constructor2
+{
+    class Constructors
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("Harry Potter", "JK Rowling", 400);
+            //book1.title = "Harry Potter";
+           // book1.author = "JK Rowling";
+          //  book1.pages = 400;
+
+            Book book2 = new Book("Lord of the Rings", "Tolkien", 800);
+            //book2.title = "Lord of the Ring";
+            // book2.author = "Tolkein";
+            // book2.pages = 800;
+
+            Book book3 = new Book();
+
+            book2.title = "The Hobbit";
+
+            Console.WriteLine(book2.title);
+
+            Console.ReadLine();
+        }
+    }
+}
