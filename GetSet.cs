b36e47425488156273f37568db6aa7a326b64e17
Argho Das <aiub2016@yahoo.com>
2021-10-10 20:43:19 +0600

    Getter Setter Source Code

---
+++ GetSet.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Get_Set
+{
+    class GetSet
+    {
+        static void Main(string[] args)
+        {
+            Movie Avengers = new Movie("The Avengers", "Iron Man", "PG-13");
+            Movie Spiderman = new Movie("No Way Home", "Spider Man", "PG");
+            //G, PG, PG-13, R, NR
+            //Avengers.rating = "Dog";
+            Avengers.Rating = "Cat";
+            Console.WriteLine(Avengers.Rating);
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Movie.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Get_Set
+{
+    class Movie
+    {
+        public string title;
+        public string hero;
+        //public string rating;
+        private string rating;
+
+        public Movie(string aTitle, string aHero, string aRating)
+        {
+            this.title = aTitle;
+            this.hero = aHero;
+            //this.rating = aRating;
+            this.Rating = aRating; //Calling the setter instead or user can change from here
+
+        }
+        //Access and Modify 
+        public string Rating
+        {
+            get { return rating; } //Get the rating attribute
+            set { 
+                   if(value == "G" || value == "PG" || value == "PG-13" || value == "R" || value == "NR") //Value = whatever got passed, here rating
+                {
+                    rating = value;
+                }
+                else
+                {
+                    rating = "NR";
+                }
+                } //set the rating
+        }
+    }
+}
