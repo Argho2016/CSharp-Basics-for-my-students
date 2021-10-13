ff761f2f3955acb2813c8bf8065f7a90f3f62f42
Argho Das <aiub2016@yahoo.com>
2021-10-13 14:02:43 +0600

    Exception Handling Source Code

---
+++ Exception.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Exep_C
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try 
+            {
+                int num1 = Convert.ToInt32(Console.ReadLine());
+
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(num1 / num2);
+            }
+            catch(DivideByZeroException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch(FormatException g)
+            {
+                Console.WriteLine(g.Message);
+            }
+            catch(Exception a)
+            {
+                Console.WriteLine(a.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Good Work");
+            }
+
+           
+        }
+    }
+}
